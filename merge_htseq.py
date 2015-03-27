5809f0dcce2f196bd0e74275db6ec28ed39bdd60
Craig Monger <craig@bioinfo-wstation-01.nibrt.ie>
2015-03-27 09:50:25 +0000

    Added parser and dictionary builder

--- merge_htseq.py
+++ merge_htseq.py
@@ -10,6 +10,24 @@ file_number= len(sys.argv)-1
 file_names=list(sys.argv)
 del file_names[0]
 
-#
+#Create a dictionary for each file
+
 for file in file_names:
-	print file 
+	file={}
+
+#Start looping through the files to extract the gene names and counts and store into dictionarys
+
+for file in file_names:
+	d = {}
+	for line in open(file):
+		value= line
+		match= re.match("^(?P<gene>[^\t]+)\t(?P<count>[^\t\n]+)\n$", value)
+		if match:
+			d[match.group("gene")] = match.group("count")
+	file = d	
+
+				
+#Loop through each gene and print the counts for each sample into a file
+
+	
+	

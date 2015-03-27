99430db0e57c8c797dbad8d77f527f391a81d375
Craig Monger <craig@bioinfo-wstation-01.nibrt.ie>
2015-03-27 07:13:55 +0000

    added method to read files

--- merge_htseq.py
+++ merge_htseq.py
@@ -1,9 +1,15 @@
+#Python script to merge to results of HtSeq into a data matrix which can be used in R
+
+#Importing modules
 import sys
 import re
 
+
+#Reading the input files
 file_number= len(sys.argv)-1
+file_names=list(sys.argv)
+del file_names[0]
 
-print file_number
-
-
-
+#
+for file in file_names:
+	print file 

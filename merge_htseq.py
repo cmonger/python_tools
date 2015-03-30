5ad5cb61d8897c168f05536fb9347b73827a9c8f
Craig Monger <craig@bioinfo-wstation-01.nibrt.ie>
2015-03-30 04:23:25 +0100

    completed script, now merges files

--- merge_htseq.py
+++ merge_htseq.py
@@ -3,31 +3,37 @@
 #Importing modules
 import sys
 import re
-
+import os
 
 #Reading the input files
-file_number= len(sys.argv)-1
+#file_number= len(sys.argv)-1
 file_names=list(sys.argv)
 del file_names[0]
 
-#Create a dictionary for each file
-
-for file in file_names:
-	file={}
-
 #Start looping through the files to extract the gene names and counts and store into dictionarys
-
+d={}
 for file in file_names:
-	d = {}
+	d[file]={}
 	for line in open(file):
 		value= line
 		match= re.match("^(?P<gene>[^\t]+)\t(?P<count>[^\t\n]+)\n$", value)
 		if match:
-			d[match.group("gene")] = match.group("count")
-	file = d	
+			geneStr= match.group("gene")
+			geneStr= str(geneStr)
+			geneCount= match.group("count")
+			geneCount= str(geneCount)
+			d[file][geneStr]=geneCount
 
-				
+	
 #Loop through each gene and print the counts for each sample into a file
+f1=open('./htseqcounts.txt', 'w+')
+for file in d:
+	f1.write('\t' + file)
+f1.write('\n')
+for gene in d[d.keys()[0]]:
+	f1.write(gene + '\t')
+	for file in d:
+		f1.write(str(d[file][gene]) + '\t')
+	f1.write('\n') 
 
-	
-	
+
